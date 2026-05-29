7aff9fd0272fedd6e84bf7f788cdbb849138fdc2
LoveAlways1 <loveprettyuz@gmail.com>
2026-05-29 16:31:07 +0100

    Fix catalog database password generation

--- terraform/live/database-passwords.tf
+++ terraform/live/database-passwords.tf
@@ -1,7 +1,6 @@
 resource "random_password" "catalog_db" {
-  length           = 24
-  special          = true
-  override_special = "!#$%&*()-_=+[]{}<>:?"
+  length  = 24
+  special = false
 }
 
 resource "random_password" "orders_db" {
